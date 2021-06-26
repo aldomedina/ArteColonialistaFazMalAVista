6f1ad579c915b1951b9b758f302038f6dfb7bae0
Aldo <aldo@studiographene.com>
2021-06-26 18:46:38 +0100

    tensorflow detecting!

--- pages/eurocentrismo.jsx
+++ pages/eurocentrismo.jsx
@@ -1,11 +1,122 @@
-import dynamic from "next/dynamic";
+import { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import Webcam from "react-webcam";
+import useAnimationFrame from "../components/Hooks/useAnimationFrame";
 
-const ObjectDetector = dynamic(() => import("../components/ObjectDetector"), {
-  ssr: false,
-});
+function Eurocentrism() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [detections, setDetections] = useState([]);
 
-const EurocentrismPage = () => {
-  return <ObjectDetector />;
-};
+  useEffect(() => {
+    runCoco();
+  }, []);
+  // Main function
+  const runCoco = async () => {
+    const net = await cocossd.load();
+    console.log("Handpose model loaded.");
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
 
-export default EurocentrismPage;
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const predictions = await net.detect(video);
+      setDetections(predictions);
+    }
+  };
+
+  useEffect(() => {
+    console.log(detections);
+  }, [detections]);
+
+  useAnimationFrame(() => grabFrame());
+
+  const grabFrame = () => {
+    if (!canvasRef || !webcamRef) return;
+    const videoInput = webcamRef.current.video;
+    const canvas = canvasRef.current;
+    if (!canvas || !videoInput) return;
+    const ctx = canvas.getContext("2d");
+    detections &&
+      !!detections.length &&
+      detections.forEach((prediction) => {
+        // Extract boxes and classes
+        const [x, y, width, height] = prediction["bbox"];
+        const text = prediction["class"];
+
+        // Set styling
+        const color = Math.floor(Math.random() * 16777215).toString(16);
+        ctx.strokeStyle = "#" + color;
+        ctx.font = "18px Arial";
+
+        // Draw rectangles and text
+        ctx.beginPath();
+        ctx.fillStyle = "#" + color;
+        ctx.fillText(text, x, y);
+        ctx.rect(x, y, width, height);
+        ctx.stroke();
+      });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          muted={true}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default Eurocentrism;
